0275aba9923700b16080dd70b056ed22e48e41de
Youssef Lourayad <youssef@expensify.com>
2024-09-30 23:51:54 +0100

    Configure fidget notifications

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -28,8 +28,18 @@ return {
                 'j-hui/fidget.nvim',
                 opts = {
                     notification = {
-                        override_vim_notify = true
-                    }
+                        override_vim_notify = true,
+                        window = {
+                            normal_hl = "Comment", -- Base highlight group in the notification window
+                            winblend = 0,          -- Background color opacity in the notification window
+                            border = "shadow",     -- Border around the notification window
+                            zindex = 45,           -- Stacking priority of the notification window
+                            x_padding = 1,         -- Padding from right edge of window boundary
+                            y_padding = 1,         -- Padding from bottom edge of window boundary
+                            align = "top",         -- How to align the notification window
+                            relative = "editor",   -- What the notification window position is relative to
+                        },
+                    },
                 }
             },
 
@@ -93,7 +103,7 @@ return {
                             source = true,
                             border = 'rounded',
                             focusable = true,
-                            scope = 'cursor'
+                            scope = 'cursor',
                         },
                         signs = {
                             text = {
