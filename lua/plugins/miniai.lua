0288d2b3ef473c1deb6414d72fd3aff44538579b
Youssef Lourayad <youssef@expensify.com>
2024-10-15 18:13:20 +0100

    Fix miniai

--- lua/plugins/miniai.lua
+++ lua/plugins/miniai.lua
@@ -1,4 +1,25 @@
 return {
-  'echasnovski/mini.ai',
-  version = false,
+  {
+    'echasnovski/mini.ai',
+    event = 'VeryLazy',
+    opts = function()
+      local miniai = require 'mini.ai'
+
+      return {
+        n_lines = 300,
+        custom_textobjects = {
+          f = miniai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+          -- Whole buffer.
+          g = function()
+            local from = { line = 1, col = 1 }
+            local to = {
+              line = vim.fn.line '$',
+              col = math.max(vim.fn.getline('$'):len(), 1),
+            }
+            return { from = from, to = to }
+          end,
+        },
+      }
+    end,
+  },
 }
