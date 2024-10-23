7a8bbc79d48f1bc7f76c0b5b9441f138661b6d33
Youssef Lourayad <youssef@expensify.com>
2024-10-23 20:13:14 +0100

    Auto close git window

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,4 +1,18 @@
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = 'fugitive',
-    command = 'normal! <ESC>'
-})
+local function quit()
+    local bufnr = vim.api.nvim_get_current_buf()
+    if vim.bo[bufnr].modifiable then
+        vim.cmd('wq')
+    else
+        vim.cmd('q!')
+    end
+
+    vim.defer_fn(function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.bo[buf].filetype == 'git' then
+                vim.api.nvim_buf_delete(buf, { force = true })
+            end
+        end
+    end, 10)
+end
+
+vim.keymap.set('n', '<M-q>', quit, { noremap = true, buffer = 0 })
