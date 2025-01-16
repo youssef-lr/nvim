3347612545e2c5e835ce497d29242fe65affe69d
Youssef Lourayad <youssef@expensify.com>
2025-01-17 00:50:53 +0100

    Add some plugins

---
+++ lua/plugins/beepboop.lua
@@ -0,0 +1,11 @@
+return {
+  'EggbertFluffle/beepboop.nvim',
+  opts = {
+    audio_player = 'afplay',
+    max_sounds = 20,
+    sound_map = {
+      { auto_command = 'VimEnter',      sound = 'chestopen.oga' },
+      { auto_command = 'InsertCharPre', sounds = { 'stone1.oga', 'stone2.oga', 'stone3.oga' } }
+    }
+  }
+}
---
+++ lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = true
+      }
+    }
+  },
+  -- stylua: ignore
+  keys = {
+  },
+}
