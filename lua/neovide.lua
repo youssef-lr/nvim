4ebb23450032b15d4f32c33700f775d1310ad4a7
Youssef Lourayad <youssef@expensify.com>
2025-12-29 20:47:02 +0100

    Animation config

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -28,15 +28,15 @@ if vim.g.neovide then
     vim.g.neovide_scale_factor = 1.0
   end)
 
-  vim.g.neovide_cursor_animation_length = 0.02
+  vim.g.neovide_cursor_animation_length = 0.05
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = false
 
   vim.g.neovide_cursor_trail_size = 0
-  vim.g.neovide_position_animation_length = 0.1
+  vim.g.neovide_position_animation_length = 0.3
 
   local scroll_animation_far_lines = 0.1
-  local scroll_animation_length = 0.1
+  local scroll_animation_length = 0.2
 
   vim.g.neovide_scroll_animation_far_lines = scroll_animation_far_lines
   vim.g.neovide_scroll_animation_length = scroll_animation_length
