58b092c02c6ea52c36daac5f552687d55ab6c04b
Youssef Lourayad <youssef@expensify.com>
2026-01-12 08:05:40 +0100

    Never show ds_store

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -104,7 +104,7 @@ return {
 
         -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
-          return false
+          return name == '.DS_Store'
         end,
 
         -- Sort file names in a more intuitive order for humans. Is less performant,
